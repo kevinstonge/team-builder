1e4e67c778ddef20e358f6985c896d5dc1cec17e
Kevin St.Onge <kevinstonge@gmail.com>
2020-08-08 09:59:16 -0400

    members display complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import membersData from "./members.js";
 
 function App() {
+  const [members, setMembers] = useState(membersData);
+  const addMember = (memberData) => {
+    console.log("add");
+    setMembers([...members, memberData]);
+  };
+  const deleteMember = (memberId) => {
+    console.log("delete");
+    let tempMembers = members;
+  };
+  const editMember = (memberId) => {
+    console.log("edit");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Team Members</h1>
       </header>
+      <div className="addMember">
+        <button className="addMemberButton">add</button>
+      </div>
+      <div className="members">
+        {members.length > 0 ? (
+          members.map((m) => (
+            <div className="member" key={m.email}>
+              <div className="memberName">
+                <h2>{m.name}</h2>
+              </div>
+              <div className="memberEmail">
+                <p>email: {m.email}</p>
+              </div>
+              <div className="memberRole">
+                <p>role: {m.role}</p>
+              </div>
+              <div className="changeMember">
+                <button className="editMemberButton">edit</button>
+                <button className="deleteMemberButton">delete</button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>no members to display</p>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/members.js
@@ -0,0 +1,27 @@
+const memberData = [
+  {
+    id: 0,
+    name: "Kevin",
+    email: "kevinstonge@gmail.com",
+    role: "front-end developer",
+  },
+  {
+    id: 1,
+    name: "Kevin",
+    email: "kevinstonge@gmail.com",
+    role: "front-end developer",
+  },
+  {
+    id: 2,
+    name: "Kevin",
+    email: "kevinstonge@gmail.com",
+    role: "front-end developer",
+  },
+  {
+    id: 3,
+    name: "Kevin",
+    email: "kevinstonge@gmail.com",
+    role: "front-end developer",
+  },
+];
+export default memberData;
